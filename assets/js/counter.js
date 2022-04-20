bb37daf480c4fe79da51b50ba9bc17c27c5d5bc9
Sanjit01 <sanjitthakuri6750@gmail.com>
2022-04-20 18:08:31 +0545

    Counter limit according to time

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -2,28 +2,33 @@ const getNumber = document.querySelectorAll(".counter .number");
 const getInput = document.querySelector("#guess_number");
 const setBtn = document.querySelector(".set-btn");
 // const resetBtn = document.querySelector(".restart");
-let winnerNumber = [5, 10, 15, 20];
+let winnerNumber = [0, 0, 0, 0];
+
+let loopIteration;
 
 let countInterval = setInterval(function () {
+  const currentCount = getCurrentCount();
+
+  //condition to return if the counter matches the loop iteration
+  if (currentCount == loopIteration) {
+    return;
+  }
+
+  // flipping animation through settimeout
   doFlip(3);
 }, 1000);
 
-// resetBtn.addEventListener("click", function (e) {
-//   e.preventDefault();
-//   clearInterval(countInterval);
-//   getNumber.forEach((num) => num.setAttribute("data-number", 0));
-//   countInterval = setInterval(function () {
-//     doFlip(3);
-//   }, 1000);
-// });
+// dynamically setting loop iteration value
+setBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  loopIteration = getInput.value;
+});
 
 function checkWiner(currentCount, winnerNumber = []) {
   if (!currentCount && !winnerNumber.length) return;
 
   winnerNumber.forEach((number) => {
-    console.log(number);
     if (currentCount === number) {
-      console.log(winnerNumber);
       document.querySelector("#modal-container").classList.remove("out");
       document.querySelector("#modal-container").classList.add("one");
       document.querySelector(".counter").classList.remove("modal-hide");
@@ -83,10 +88,6 @@ function doFlip(numberIndex) {
   }, 500);
 
   const currentCount = getCurrentCount() + 1;
-  // console.log(currentCount);
-  // setBtn.addEventListener("click", function (e) {
-  // });
-  // checkWiner(currentCount, winnerNumber);
 
   checkWiner(currentCount, winnerNumber);
 }
