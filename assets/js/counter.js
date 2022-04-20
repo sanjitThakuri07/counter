88e10c44866fcd9e0ac3cf2612e507965a4c4026
Sanjit01 <sanjitthakuri6750@gmail.com>
2022-04-20 17:32:17 +0545

    Fixed

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,40 +1,51 @@
 const getNumber = document.querySelectorAll(".counter .number");
 const getInput = document.querySelector("#guess_number");
 const setBtn = document.querySelector(".set-btn");
-const resetBtn = document.querySelector(".restart");
-let winnerNumber;
+// const resetBtn = document.querySelector(".restart");
+let winnerNumber = [5, 10, 15, 20];
 
 let countInterval = setInterval(function () {
   doFlip(3);
 }, 1000);
 
-resetBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  clearInterval(countInterval);
-  getNumber.forEach((num) => num.setAttribute("data-number", 0));
-  countInterval = setInterval(function () {
-    doFlip(3);
-  }, 1000);
-});
+// resetBtn.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   clearInterval(countInterval);
+//   getNumber.forEach((num) => num.setAttribute("data-number", 0));
+//   countInterval = setInterval(function () {
+//     doFlip(3);
+//   }, 1000);
+// });
 
-function checkWiner(currentCount, setNumber) {
-  if (!currentCount && !setNumber) return;
+function checkWiner(currentCount, winnerNumber = []) {
+  if (!currentCount && !winnerNumber.length) return;
 
-  if (currentCount + 1 === +setNumber) {
-    clearInterval(countInterval);
-    document.querySelector("#modal-container").classList.add("one");
-  } else if (currentCount + 1 > +setNumber) {
-    clearInterval(countInterval);
-    alert("THe number you have set is less than the timer");
-  } else {
-    document.querySelector("#modal-container").classList.remove("one", "out");
-  }
+  winnerNumber.forEach((number) => {
+    console.log(number);
+    if (currentCount === number) {
+      console.log(winnerNumber);
+      document.querySelector("#modal-container").classList.remove("out");
+      document.querySelector("#modal-container").classList.add("one");
+      document.querySelector(".counter").classList.remove("modal-hide");
+      document.querySelector(".counter").classList.add("modal-show");
+    }
+  });
+
+  // if (currentCount + 1 === ) {
+  //   document.querySelector("#modal-container").classList.add("one");
+  // } else if (currentCount + 1 > +setNumber) {
+  //   alert("THe number you have set is less than the timer");
+  // } else {
+  // }
 }
 
 document
   .querySelector(".modal-background")
   .addEventListener("click", function (e) {
+    document.querySelector("#modal-container").classList.remove("one");
     document.querySelector("#modal-container").classList.add("one", "out");
+    document.querySelector(".counter").classList.remove("modal-show");
+    document.querySelector(".counter").classList.add("modal-hide");
   });
 
 function getCurrentCount() {
@@ -71,10 +82,11 @@ function doFlip(numberIndex) {
     currentNumberElement.removeClass("flip");
   }, 500);
 
-  const currentCount = getCurrentCount();
-  setBtn.addEventListener("click", function (e) {
-    e.preventDefault();
-    winnerNumber = getInput.value;
-  });
+  const currentCount = getCurrentCount() + 1;
+  // console.log(currentCount);
+  // setBtn.addEventListener("click", function (e) {
+  // });
+  // checkWiner(currentCount, winnerNumber);
+
   checkWiner(currentCount, winnerNumber);
 }
